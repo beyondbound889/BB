d2213b135005b98758320c091ac9113ac6d408a5
Raman <103361956+Ramanbansal2004@users.noreply.github.com>
2025-10-08 21:52:00 +0530

    o

--- ayurhelix/src/components/stats.jsx
+++ ayurhelix/src/components/stats.jsx
@@ -42,10 +42,10 @@ const StatItem = ({ label, target, suffix = '', fraction = false, url=""}) => {
 
 const DiabetesStats = () => (
   <div className="bg-[#dec493] py-8 px-2 md:px-10 flex flex-wrap justify-center items-center gap-y-4">
-    <StatItem label="Patients kidney Decreased after uses metformin" target={98} suffix="%+" fraction url="https://pmc.ncbi.nlm.nih.gov/articles/PMC5797060/" />
-    <StatItem label="Pancreas side affects after using some allopathic anti diabetic medicine" target={80} suffix='%+' url="https://pmc.ncbi.nlm.nih.gov/articles/PMC9716078/"/>
-    <StatItem label="Patients show B12 deficiency after using metformin" target={28} suffix="%+" url="https://www.sciencedirect.com/science/article/pii/S2352385917302657?utm_source=chatgpt.com"/>
-    <StatItem label="Heart Failure using some Allopathic Antidiabetic medicine" url="https://www.acc.org/sitecore/content/Sites/ACC/Home/Latest-in-Cardiology/Articles/2019/12/10/12/18/What-Percentage-of-Diabetes-Patients-Taking-TZDs-Show-Signs-of-HF" target={38} suffix="%+" />
+    <StatItem label="Showed kidney size reduction with metformin" target={98} suffix="%+" fraction url="https://pmc.ncbi.nlm.nih.gov/articles/PMC5797060/" />
+    <StatItem label="Showed pancreatic side effects with some allopathic diabetes drugs" target={80} suffix='%+' url="https://pmc.ncbi.nlm.nih.gov/articles/PMC9716078/"/>
+    <StatItem label="Patients showed b12 deficiency after using metformin" target={28} suffix="%+" url="https://www.sciencedirect.com/science/article/pii/S2352385917302657?utm_source=chatgpt.com"/>
+    <StatItem label="Experienced heart failure with certain allopathic anti-diabetic drugs" url="https://www.acc.org/sitecore/content/Sites/ACC/Home/Latest-in-Cardiology/Articles/2019/12/10/12/18/What-Percentage-of-Diabetes-Patients-Taking-TZDs-Show-Signs-of-HF" target={38} suffix="%+" />
   </div>
 );
 
